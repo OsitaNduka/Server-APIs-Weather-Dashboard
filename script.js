0826f2cdad704e293604e1327832fc025a9294a0
OsitaNduka <118213714+OsitaNduka@users.noreply.github.com>
2023-02-05 18:34:53 +0000

    added selectors to scripts.js

--- script.js
+++ script.js
@@ -1,19 +1,41 @@
 $(document).ready(function (){
 
-// My API Key
-var APIKey =  "f632d71609c01815c6dc91d63186fe2a";
-// The URL to query the database
-var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" +
-APIKey;
+ // My API Key
+ var APIKey =  "f632d71609c01815c6dc91d63186fe2a";
 
-// create an AJAX call
+ //Selectors for Html Elements
+ var formHeadingEl = $("#form-heading");
+ var searchFormEl = $("#search-form");
+ var searchInputEl = $("#search-input");
+ var searchButtonEl = $("#search-button");
+ var historyEl = ("#history");
+ var todayEl = $("#today");
+ var forecast = $("#forecast");
+ var cityEl = $("#city");
+ var dateEl = $("#date");
+ var weatherEl = $("#weather");
+ 
 
-$.ajax({
-    url: queryURL,
-    method: "GET",
-}).then(function (response) {
-    console.log(queryURL);
-    console.log(response);
-});
+
+ // The URL to query the database
+ var queryURL = "http://api.openweathermap.org/geo/1.0/direct?q=London&limit=5&appid=" +
+ APIKey;
+
+ // create an AJAX call
+
+    $.ajax({
+       url: queryURL,
+       method: "GET",
+     }).then(function (response) {
+     console.log(queryURL);
+     console.log(response);
+    
+    
+    });
+
+
+
+
+
 
 });
